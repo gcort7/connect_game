9ee5841db7ff2a89b6ae88967deed4b09174efd2
unknown <giovanny.cortes75@gmail.com>
2019-11-07 11:52:42 -0500

    Connect 4 game board

--- src/App.js
+++ src/App.js
@@ -1,26 +1,100 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+function Circle(props){
+  var color = "white"
+  if(props.cell == 1){
+      color = "black"
+  }
+  else if(props.cell == 2){
+      color = "red"
+  }
+  var style = {
+      backgroundColor:color,
+      border: "1px solid black",
+      borderRadius: "100%",
+      paddingTop: "98%"
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+     <div style = {style}></div>
+  )
 }
 
-export default App;
+function Cell(props){
+  var style = {
+      height:50,
+      width:50,
+      border:"1px solid black",
+      backgroundColor:"yellow"
+  }
+  return (
+    <div style = {style} onClick = {() => props.handleClick(props.row,props.col)}>
+      <Circle cell = {props.cell}/>
+    </div>
+  )
+}
+
+function Row(props){
+  var style = {
+    display: "flex"
+  }
+
+  var cells = []
+  for(let i = 0; i < 7; i++){
+    cells.push(<Cell key = {i} cell = {props.cells[i]} row = {props.row} col = {i} handleClick = {props.handleClick}/>)
+  }
+
+  return (
+    <div style = {style}>
+      {cells}
+    </div>
+  )
+
+}
+
+function Board(props){
+  var rows = []
+  for(let i = 5; i >= 0; i--){
+    rows.push(<Row key = {i} row = {i} cells = {props.cells[i]} handleClick = {props.handleClick}/>)
+  }
+  return (
+      <div>
+          {rows}
+      </div>
+  )
+}
+
+class Game extends React.Component{
+
+  constructor(props){
+    super(props)
+    var cells = []
+    for(let i = 0; i < 6; i++ ){
+    cells.push(new Array(7).fill(0))
+    }
+    this.state = {player:false, cells:cells, winner:0, test: ''} 
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick(row,col){
+    console.log("row: " + row + " | col: " + col)
+    console.log(this.state.cells)
+    var temp = [];
+    for(let i = 0; i < 6; i++){
+      temp.push(this.state.cells[i].slice())
+    }
+    temp[row][col] = this.state.player? 1 : 2
+    this.setState({cells:temp, player: !this.state.player})
+}
+
+  render(){
+    return (
+      <div>
+        <h1>{this.state.player? "Blacks Turn" : "Red Turn"}</h1>
+          <Board cells = {this.state.cells} handleClick = {this.handleClick}/>
+          <button>Restart</button>
+      </div>
+      )
+  }
+}
+
+export default Game;
